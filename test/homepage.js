c24591d71992b1f008b925f31d9abd60b5960941
Danzil Fernandes <dfernandes@teacherspayteachers.com>
2019-06-05 13:45:41 +0530

    Initial commit

---
+++ test/homepage.js
@@ -0,0 +1,92 @@
+var HomePage = require('./Homepage.page.js');
+const assert= require ('assert');
+
+describe ('Test homepage', () => {
+    it ('should get the correct page title', () => {
+        browser.url ('/');
+        const title=browser.getTitle();
+        assert.equal(title,'Home Page | SJ Innovation LLC');
+        
+    });
+
+    it('should verify the height of the homepage image', () =>{
+        assert.equal(HomePage.heightoftheimage(),579);
+        assert.equal(HomePage.widthoftheimage(),1300);
+    });
+
+    it('should demonstrate the scrollIntoView command', () => {
+        HomePage.scrollsthepageDown();
+        HomePage.scrollsthepageUp();
+    });
+
+    it('should verify proper text is displayed in the body of the page', () => {
+        assert.equal (HomePage.textonthepage(), "We're an agile team of experts who take a strategic approach to deliver results. We produce original and thoughtful pieces of design for businesses throughout Hampshire.");
+    });
+
+    it('should display Our Services on the homepage',() => {
+        //const selector= $('.row.our_services');
+        HomePage.displayourservices();
+    });
+
+    it('should display Our Clients on the homepage',() => {
+        //const selector= $('.row.our_services');
+        HomePage.displayourclient();
+    });
+
+    it('should display "Get a free Quote now" button', () => {
+        //$('.button-holder .svg-btn').isVisible();
+        HomePage.ourqouteBtn();
+    });
+
+    it('should verify if the dropdown is visible on clicking Services', () => {
+        HomePage.dropdownisvisible();
+    });
+
+    it('should verify the drop down options for Services', () => {
+        assert.equal(HomePage.checkdropdownvalue1(),'Web Application');
+        assert.equal(HomePage.checkdropdownvalue2(),'Design');
+        assert.equal(HomePage.checkdropdownvalue3(),'Mobile Application');
+        assert.equal(HomePage.checkdropdownvalue4(),'Quality Assurance');
+        assert.equal(HomePage.checkdropdownvalue5(),'HR Sourcing Solution');
+    })
+
+
+    it('should redirect to Our Portfolio page', () => {
+        HomePage.clickourPortfolio();
+    });
+
+    it('should redirect to About Us page', () => {
+        HomePage.clickaboutUs();
+        HomePage.scrollsthepageDown();
+        HomePage.scrollsthepageUp();
+    });
+
+    it('should redirect to Services(QA) page', () => {
+        HomePage.clickServices();
+        HomePage.scrollsthepageDown();
+        HomePage.scrollsthepageUp();
+    });
+
+    it('should redirect to Blog page', () => {
+        HomePage.clickBlog();
+        HomePage.scrollsthepageDown();
+        HomePage.scrollsthepageUp();
+
+    });
+
+    it('should redirect to Contact Us page', () => {
+        HomePage.clickcontactUs();
+        HomePage.scrollsthepageDown();
+        HomePage.scrollsthepageUp();
+    });
+
+    it('should redirect to Careers page', () => {
+       HomePage.clickCareers();
+       HomePage.scrollsthepageDown();
+        HomePage.scrollsthepageUp();
+    });
+
+    
+
+
+});
---
+++ test/homepage.page.js
@@ -0,0 +1,125 @@
+class Homepage {
+  get scrollDown() { return $('.row.main-quote .col-lg-12 p'); }
+  get scrollUp() { return $("#wpfront-scroll-top-container"); }
+  get imgSize() { return $('img.attachment-full'); }
+  get text() { return $('.row.main-quote .col-lg-12 p'); }
+  get ourservices() { return $('.row.our_services'); }
+  get ourclients() { return $('.our_clients.col-lg-12');}
+  get quote() { return $('.button-holder .svg-btn');}
+  get dropdown() { return $('.dropdown-toggle');}
+  get web() { return $('#menu-item-4093');}
+  get design() { return $('#menu-item-4094');}
+  get mobile() { return $('#menu-item-4095');}
+  get quality() { return $('#menu-item-4096');}
+  get hr() { return $('#menu-item-4097');}
+  get portfolio() { return $('a[href="https://sjinnovation.com/our-portfolio/"]');}
+  get aboutUs() { return $('a[href="https://sjinnovation.com/about-us/"]');}
+  get QA() { return $('a[href="/services#qa"]');}
+  get Careers() { return $('a[href="http://career.sjinnovation.com/"]');}
+  get blog() { return $('a[href="https://sjinnovation.com/blog/"]');}
+  get contactUs() { return $('a[href="https://sjinnovation.com/contact-us/"]');}
+
+
+ 	scrollsthepageDown(){
+
+		return this.scrollDown.scroll(0,800);
+		
+	}
+
+  	scrollsthepageUp()
+	{
+		return this.scrollUp.click();
+	}
+
+	heightoftheimage()
+	{	
+		//console.log(this.imgSize.getElementSize('height'));
+		return this.imgSize.getElementSize('height');
+
+	}
+
+	widthoftheimage()
+	{
+		//onsole.log(this.imgSize.getElementSize('width'));
+		return this.imgSize.getElementSize('width');
+	}
+
+	textonthepage()
+	{
+		return this.text.getText();
+	}
+
+	displayourservices(){
+		return this.ourservices.isVisible();
+
+	}
+
+	displayourclient(){
+		return this.ourclients.isVisible();
+	}
+
+	ourqouteBtn(){
+		return this.quote.isVisible();
+	}
+
+	dropdownisvisible(){
+		this.dropdown.click();
+		this.dropdown.waitForVisible(3000);
+		this.dropdown.isVisible();
+	}
+
+	checkdropdownvalue1()
+	{
+		return this.web.getText();
+	}
+
+	checkdropdownvalue2()
+	{
+		return this.design.getText();
+	}
+
+	checkdropdownvalue3()
+	{
+		return this.mobile.getText();
+	}
+
+	checkdropdownvalue4()
+	{
+		return this.quality.getText();
+	}
+
+	checkdropdownvalue5()
+	{
+		return this.hr.getText();
+	}
+
+	clickourPortfolio(){
+		return this.portfolio.click();
+	}
+
+	clickaboutUs(){
+		return this.aboutUs.click();
+	}
+
+	clickServices(){
+		this.dropdown.click();
+		this.QA.click();
+	}
+
+	clickCareers(){
+		this.Careers.click();
+	}
+
+	clickBlog(){
+		this.blog.click();
+	}
+
+	clickcontactUs(){
+		this.contactUs.click();
+	}
+
+
+
+};
+
+module.exports = new Homepage();
